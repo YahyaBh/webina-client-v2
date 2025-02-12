b48a09188686c0fb8c9adf6fc0717e142f6a8d6a
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2025-02-12 23:41:46 +0100

    Update Reserve

---
+++ src/app/reserve/page.jsx
@@ -0,0 +1,34 @@
+import './page.scss'
+import Navbar from '../Layouts/Navbar/Navbar'
+
+const Reserve = () => {
+    return (
+        <>
+
+            <header>
+                <Navbar target={"reserve"} />
+
+
+                <div className='container'>
+
+                    <div className='glow-box'></div>
+                    <h2>Request everything you want to create and leave ️webina digital  it on the application</h2>
+
+                    <div className='bottom'>
+                        <img className='left-img' src='/Images/Reserve/HeaderHand.svg' />
+
+                        <button>CONSULTE FOR FREE!</button>
+
+                        <img className='right-img' src='/Images/Reserve/HeaderHand.svg' />
+                    </div>
+
+                </div>
+
+            </header>
+
+
+        </>
+    )
+}
+
+export default Reserve
