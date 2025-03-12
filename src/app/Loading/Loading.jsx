c198ce46403bc4ccfebf41f2768250c5df64edf1
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2025-03-12 22:48:28 +0000

    Code sharp

--- src/app/Loading/Loading.jsx
+++ src/app/Loading/Loading.jsx
@@ -1,19 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react'
-// import Logo from '../../../public/assets/Home/Navbar/WEBINA2.png'
+"use client"
+import React from 'react'
 import LogoD from '../../../public/assets/Home/Navbar/WEBINA-Logo.png'
 
 
 import './Loading.scss'
 import Image from 'next/image'
-// import { ThemeContext } from '../Context/ThemeContext'
 
 const Loading = () => {
-
-    // const { isDarkMode } = useContext(ThemeContext);
-
-    // useEffect(() => {
-    //     isDarkMode ? setCurrentImage(LogoD) : setCurrentImage(Logo)
-    // }, [isDarkMode])
 
 
 
