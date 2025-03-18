adc2ad95aa351c46704c054b3e6262f27fc409e8
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2025-03-18 15:22:21 +0000

    Update client.js

--- src/app/client.js
+++ src/app/client.js
@@ -4,6 +4,12 @@ import CustomCursor from './lib/Cursor';
 
 const ClientWrapper = ({ children }) => {
 
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            document.body.style.overflow = 'hidden';
+        }
+    }, []);
+
     return <div>
         <CustomCursor />
         {children}
