74b14d0b9b8e06fe262c3a4a9df15eb5f11967b1
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2025-03-22 23:35:22 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   images: {
     domains: ["cdn.sanity.io"],
   },
